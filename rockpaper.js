d3514d1a7fa6ff5d9c180ddea1499825d74286d9
Iain <iain.brookes@outlook.com>
2020-09-01 19:12:49 +0100

    Moved JS into seperate file and preparing for html overhaul as per the TOP assignment.

---
+++ rockpaper.js
@@ -0,0 +1,51 @@
+function computerPlay() {
+
+    let randomNumber = (Math.floor(Math.random() * 3));
+
+    if (randomNumber === 0) {
+        console.log('Computer has chosen rock!');
+        return "rock";
+    } else if (randomNumber === 1) {
+        console.log('Computer has chosen paper!');
+        return "paper";
+    } else if (randomNumber === 2) {
+        console.log('Computer has chosen scissors!');
+        return "scissors";
+    }
+}
+
+let computerSelection = computerPlay();
+
+let userInput = prompt('Please enter your pick of poison.');
+
+function playerSelection(userInput) {
+    return userInput.toLowerCase();
+}
+
+function playRound(computerSelection, playerSelection) {
+    if (computerSelection == "rock" && playerSelection == "rock") {
+        return "It's a tie.";
+    } else if (computerSelection == "paper" && playerSelection == "rock") {
+        return "Computer wins!";
+    } else if (computerSelection == "scissors" && playerSelection == "rock") {
+        return "Player wins!";
+    } else if (computerSelection == "scissors" && playerSelection == "scissors") {
+        return "It's a tie.";
+    } else if (computerSelection == "rock" && playerSelection == "scissors") {
+        return "Computer wins!";
+    } else if (computerSelection == "paper" && playerSelection == "scissors") {
+        return "Player wins!";
+    } else if (computerSelection == "paper" && playerSelection == "paper") {
+        return "It's a tie.";
+    } else if (computerSelection == "scissors" && playerSelection == "paper") {
+        return "Computer wins!";
+    } else if (computerSelection == "rock" && playerSelection == "paper") {
+        return "Player wins!";
+    }
+}
+
+function playGame() {
+    alert(playRound(computerSelection, playerSelection(userInput)));
+}
+
+playGame();
